e84504a1c9e3d978cf9f45669e434737ea89c663
Geraldine <geraldine.orellana@gmail.com>
2022-11-05 23:11:29 -0300

    update employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,14 @@ export class AppComponent {
   }
 
   addOrEdit() {
-    this.selectedEmployee.id = this.employeeArray.length + 1 //genereamos nuevo id 
-    this.employeeArray.push(this.selectedEmployee); //para que el nuevo employee se agregue al final de todo del arreglo, el empleado que estoy llenando (selected) se guarda dentro del arreglo
+
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1 //genereamos nuevo id 
+      this.employeeArray.push(this.selectedEmployee); //para que el nuevo employee se agregue al final de todo del arreglo, el empleado que estoy llenando (selected) se guarda dentro del arreglo
+
+    }
+
+
     this.selectedEmployee = new Employee(); //cuando agregamos uno nuevo new employee queda vacio
 }
   
