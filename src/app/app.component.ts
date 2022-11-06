f5311c6b5c1cb3312a08244df0929629c7129f45
Geraldine <geraldine.orellana@gmail.com>
2022-11-05 22:37:35 -0300

    create and select new employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud';
+
+  employeeArray: Employee[] = [
+    {id: 1, name: "Ryan", country: "USA"},
+    {id: 2, name: "Angelica", country: "USA"},
+    {id: 1, name: "Joe", country: "USA"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    this.selectedEmployee.id = this.employeeArray.length + 1 //genereamos nuevo id 
+    this.employeeArray.push(this.selectedEmployee); //para que el nuevo employee se agregue al final de todo del arreglo, el empleado que estoy llenando (selected) se guarda dentro del arreglo
+    this.selectedEmployee = new Employee(); //cuando agregamos uno nuevo new employee queda vacio
 }
+  
+  }
